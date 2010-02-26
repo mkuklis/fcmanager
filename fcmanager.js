a01f92f08bdc02e7889f70f5f54a0a24e6590f34
michal <michal@ubi.(none)>
2010-02-26 01:49:50 -0500

    fixed description

--- fcmanager.js
+++ fcmanager.js
@@ -15,8 +15,7 @@
 
 (function($) {
 			
-	$.fn.fcManager = function(options) {
-	
+  $.fn.fcManager = function(options) {
 		o = $.extend({}, o, options);
 		
 		return this.each(function() {
@@ -181,19 +180,19 @@
 	// utils 
 	
 	$.fn.serializeObject = function() {
-    var o = {};
-    var a = this.serializeArray();
-    $.each(a, function() {
-        if (o[this.name]) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
+		var o = {};
+		var a = this.serializeArray();
+		$.each(a, function() {
+			if (o[this.name]) {
+				if (!o[this.name].push) {
+					o[this.name] = [o[this.name]];
+				}
+        o[this.name].push(this.value || '');
+			} else {
+				o[this.name] = this.value || '';
+			}
+		});
+		return o;
 	};
 		
 })(jQuery);
